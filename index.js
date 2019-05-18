ae5f8c3577d6dc596eedbb28140c5987111b4545
Pharuxtan <alex360noscop@gmail.com>
2019-05-18 15:44:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -299,7 +299,7 @@ client.on("message", message => {
 					var dates = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
 					var year = a.getUTCFullYear();
 					var month = months[a.getUTCMonth()];
-					var date = dates[a.getUTCDate()-2];
+					var date = dates[a.getUTCDate()-1];
 					var time = `${year}${month}${date}`;
 					const embed = new Discord.RichEmbed()
 						.setColor(0x009688)
