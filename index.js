5f15b111af2fc3bb64ee0335ba1e12fa9e0caf3d
Pharuxtan <alex360noscop@gmail.com>
2019-07-14 18:42:41 +0200

    Added MSM Xtended

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if(langfile[configlang] === undefined){
 //end
 var lang = langfile[configlang];
 const client = new Discord.Client();
-const roms = ["DotOS (dotos)\n", "Evolution-X (evo/evox)\n", "HavocOS (havoc)\n", "PearlOS (pearl)\n", "PixysOS (pixy)\n", "Potato Open Sauce Project (posp/potato)\n", "ViperOS (viper)\n", "LineageOS (los/lineage)\n", "Pixel Experience (pe)\n", "BootleggersROM (btlg/bootleggers)\n", "AOSP Extended (aex)\n", "crDroid (crdroid)\n", "Syberia (syberia)\n", "Clean Open Source Project (cosp/clean)\n", "Resurrection Remix (rr)\n", "SuperiorOS (superior)\n", "RevengeOS (revenge)\n", "Android Open Source illusion Project (aosip)\n", "ArrowOS (arrow)\n", "Liquid Remix (liquid)\n", "Dirty Unicorns (dirty)\n", "XenonHD (xenon)\n", "Kraken Open Tentacles Project (kotp/kraken)\n", "Android Ice Cold Project (aicp)\n", "NitrogenOS (nitrogen)\n", "CerberusOS (cerberus)\n"].sort(function (a, b) {return a.toLowerCase().localeCompare(b.toLowerCase())}).join('');
+const roms = ["DotOS (dotos)\n", "Evolution-X (evo/evox)\n", "HavocOS (havoc)\n", "PearlOS (pearl)\n", "PixysOS (pixy)\n", "Potato Open Sauce Project (posp/potato)\n", "ViperOS (viper)\n", "LineageOS (los/lineage)\n", "Pixel Experience (pe)\n", "BootleggersROM (btlg/bootleggers)\n", "AOSP Extended (aex)\n", "crDroid (crdroid)\n", "Syberia (syberia)\n", "Clean Open Source Project (cosp/clean)\n", "Resurrection Remix (rr)\n", "SuperiorOS (superior)\n", "RevengeOS (revenge)\n", "Android Open Source illusion Project (aosip)\n", "ArrowOS (arrow)\n", "Liquid Remix (liquid)\n", "Dirty Unicorns (dirty)\n", "XenonHD (xenon)\n", "Kraken Open Tentacles Project (kotp/kraken)\n", "Android Ice Cold Project (aicp)\n", "NitrogenOS (nitrogen)\n", "CerberusOS (cerberus)\n", "MSM Xtended (msm)\n"].sort(function (a, b) {return a.toLowerCase().localeCompare(b.toLowerCase())}).join('');
 function timeConverter(timestamp){
   var a = new Date(timestamp * 1000);
   var months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
@@ -1778,6 +1778,26 @@ client.on("message", message => {
 		} else {
 			send(lang.cdnerr)
 		}
+	//MSM Xtended
+	} else if(content.startsWith(`${prefix}msm`) || content.startsWith(`${prefix}xtended`) || content.startsWith(`${prefix}msmxtended`)){
+		const codename = content.split(' ')[1];
+		if(codename !== undefined){
+			const codenameup = content.split(' ')[1].toUpperCase();
+			rom("https://raw.githubusercontent.com/Xtended-Pie/vendor_xtended_DevicesOTA/xp/ota.xml", '', false, false, false, true, false, false, false, false, codename, codenameup).then(response => {
+				if(response){
+					var filename = response.filename._text;
+					const embed = new Discord.RichEmbed()
+						.setColor(0x940000)	
+						.setTitle(`MSM Xtended | ${devicename(codename)}`)
+						.setDescription("**"+lang.date+"**: **`"+ `${filename.split('-')[2].substring(0, 4)}-${filename.split('-')[2].substring(4, 6)}-${filename.split('-')[2].substring(6, 8)}` +"`**\n**"+lang.version+"**: **`"+filename.split('-')[6].split(".")[0]+"`**\n"+`**${lang.download}**: [${filename}](${response.download._text})`)
+					send({embed});
+				} else {
+					send(lang.romerr+" `"+devicename(codename)+"`")
+				}
+			});
+		} else {
+			send(lang.cdnerr)
+		}
 	//ROMs
 	} else if(content.startsWith(`${prefix}roms`)){
 		const codename = content.split(' ')[1];
@@ -2008,6 +2028,31 @@ client.on("message", message => {
 					});
 				});
 			}
+			async function rommsm(code, codeup, name) {
+				return new Promise(function(resolve, reject) {
+					request({
+						url: "https://raw.githubusercontent.com/Xtended-Pie/vendor_xtended_DevicesOTA/xp/ota.xml"
+					}, function(err, responses, bodyurl) {
+						var body = convert.xml2json(bodyurl, {compact: true, spaces: 4});
+						function resp(){
+							try {
+								return JSON.parse(body).OTA.manufacturer.find((m) => m[codename] !== undefined)[codename] !== undefined;
+							} catch (err) {
+								try {
+									return JSON.parse(body).OTA.manufacturer.find((m) => m[codename] !== undefined)[codename] !== undefined;
+								} catch (err) {
+									return false;
+								}
+							}
+						}
+						if(resp() === false){
+							resolve(false)
+						} else {
+							resolve(`${name}\n`)
+						}
+					});
+				});
+			}
 			async function romsyb(coden, codenup, name){
 				return new Promise(function(resolve, reject) {
 					request({
@@ -2208,11 +2253,13 @@ client.on("message", message => {
 			roms(`https://raw.githubusercontent.com/nitrogen-project/OTA/p/${codename}.json`, `https://raw.githubusercontent.com/nitrogen-project/OTA/p/${codenameup}.json`, "NitrogenOS (nitrogen)").then(nitrogen => {
 			//CerberusOS
 			roms(`https://raw.githubusercontent.com/CerberusOS/official_devices/Pie/${codename}/build.json`, `https://raw.githubusercontent.com/CerberusOS/official_devices/Pie/${codenameup}/build.json`, "CerberusOS (cerberus)").then(cerberus => {
+			//MSM Xtended
+			rommsm(codename, codenameup, "MSM Xtended (msm)").then(msm => {
 				
-				//havoc, pixy, los, pearl, dotos, viper, pospw, pospm, evo, aexpie, aexoreo, btlg, pepie, pecaf, peoreo, pego, syberia, crdroid, cosp, rr, superior, revenge, aosipo, aosipb, arrow, liquid, dirtyo, dirtyr, dirtyw, xenono, xenone, kotp, aicp, nitrogen, cerberus
+				//havoc, pixy, los, pearl, dotos, viper, pospw, pospm, evo, aexpie, aexoreo, btlg, pepie, pecaf, peoreo, pego, syberia, crdroid, cosp, rr, superior, revenge, aosipo, aosipb, arrow, liquid, dirtyo, dirtyr, dirtyw, xenono, xenone, kotp, aicp, nitrogen, cerberus, msm
 				
 				
-				if(havoc === false && pixy === false && los === false && pearl === false && dotos === false && viper === false && pospw === false && pospm === false && evo === false && aexpie === false && aexoreo == false && btlg === false && pepie === false && pecaf === false && peoreo === false && syberia === false && crdroid === false && cosp === false && rr === false && pego === false && superior === false && revenge === false && aosipo === false && aosipb === false && arrow === false && liquid === false && dirtyo === false && dirtyr === false && dirtyw === false && xenone === false && xenono === false && kotp === false && aicp === false && nitrogen === false && cerberus === false){
+				if(havoc === false && pixy === false && los === false && pearl === false && dotos === false && viper === false && pospw === false && pospm === false && evo === false && aexpie === false && aexoreo == false && btlg === false && pepie === false && pecaf === false && peoreo === false && syberia === false && crdroid === false && cosp === false && rr === false && pego === false && superior === false && revenge === false && aosipo === false && aosipb === false && arrow === false && liquid === false && dirtyo === false && dirtyr === false && dirtyw === false && xenone === false && xenono === false && kotp === false && aicp === false && nitrogen === false && cerberus === false && msm === false){
          				send(lang.romserr+" `"+devicename(codename)+"`");
 				} else {
 					
@@ -2227,7 +2274,7 @@ client.on("message", message => {
 					const embed = new Discord.RichEmbed()
 						.setColor(0xFFFFFF)
 						.setTitle(`${lang.roms} ${devicename(codename)}`)
-						.setDescription([tof(havoc), tof(pixy), tof(los), tof(pearl), tof(dotos), tof(viper), tof(pospw), tof(pospm), tof(evo), tof(aexpie), tof(aexoreo), tof(btlg), tof(pepie), tof(pecaf), tof(peoreo), tof(pego), tof(syberia), tof(crdroid), tof(cosp), tof(rr), tof(superior), tof(revenge), tof(aosipo), tof(aosipb), tof(arrow), tof(liquid), tof(dirtyo), tof(dirtyr), tof(dirtyw), tof(xenono), tof(xenone), tof(kotp), tof(aicp), tof(nitrogen), tof(cerberus)].sort(function (a, b) {return a.toLowerCase().localeCompare(b.toLowerCase())}).join(''))
+						.setDescription([tof(havoc), tof(pixy), tof(los), tof(pearl), tof(dotos), tof(viper), tof(pospw), tof(pospm), tof(evo), tof(aexpie), tof(aexoreo), tof(btlg), tof(pepie), tof(pecaf), tof(peoreo), tof(pego), tof(syberia), tof(crdroid), tof(cosp), tof(rr), tof(superior), tof(revenge), tof(aosipo), tof(aosipb), tof(arrow), tof(liquid), tof(dirtyo), tof(dirtyr), tof(dirtyw), tof(xenono), tof(xenone), tof(kotp), tof(aicp), tof(nitrogen), tof(cerberus), tof(msm)].sort(function (a, b) {return a.toLowerCase().localeCompare(b.toLowerCase())}).join(''))
 					send({embed});
 				}
         message.channel.stopTyping();
